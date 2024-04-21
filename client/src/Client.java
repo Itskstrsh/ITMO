04be4ef5756b743b825fa60d2e3897c8d863c6f4
Kostya Roschin <114172232+Itskstrsh@users.noreply.github.com>
2024-04-22 01:40:50 +0300

    Delete client directory

--- client/src/Client.java
+++
@@ -1,8 +0,0 @@
-import network.Program;
-
-public class Client {
-    public static void main(String[] args) throws InterruptedException {
-        Program program = new Program();
-        program.execute();
-    }
-}
