b3930eb6e4da75aa24f25c91798d3322686a331a
kostyarsh <roshinkost@yandex.ru>
2025-09-23 21:39:51 +0300

    add .mli

---
+++ fp/lab1/namesScore.mli
@@ -0,0 +1,14 @@
+module Euler22 : sig
+  val split_csv_names : string -> string list
+  val sort_names : string list -> string list
+  val name_value : string -> int
+
+  val tail_score : string list -> int64
+  val non_tail_score : int -> string list -> int64
+  val modular_score : string list -> int64
+  val map_score : string list -> int64
+  val loop_score : string list -> int64
+  val seq_score : string list -> int64
+
+  val solution : string -> int64 * (string * int64) list
+end
